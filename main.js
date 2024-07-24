17b0c14e512a0374b12740e3653b414db872de8f
HussnainProg <husnain6908087@gmail.com>
2024-07-24 10:27:20 +0500

    second

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-// alert("HAHAAHHA");
-// console.log("asdklaskd");
-
 let ruleBtn =  document.querySelector(".rul-btn");
 ruleBtn.addEventListener("click",(e)=>{
     alert("ok");
-
-    // let ruleBox = document.getElementsByClassName("game-rules-wrapper");
-    // let box = ruleBox[0];
-    // box.computedStyleMap.
-    // ruleBox.document.
 
 }) 
